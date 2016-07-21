0bd216d7f51de4680d5f08d82d41ad1d8e1ec5b8
CockDeveloper <COCK.Developer@gmail.com>
2016-07-21 07:49:06 +0700

    Modify Output UTF8 not success

--- Exercise_Resolve_again/Program.cs
+++ Exercise_Resolve_again/Program.cs
@@ -9,6 +9,9 @@ namespace ExerciseResolve
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
             int num = 153;
             Ex001_InformationOfNumber infoNum = new Ex001_InformationOfNumber(num);
             Console.WriteLine(infoNum.ToString());
