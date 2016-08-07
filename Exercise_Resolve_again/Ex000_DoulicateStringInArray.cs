0ac2398c3008705fb0d4c373f9912cfa2dab3c80
CockDeveloper <COCK.Developer@gmail.com>
2016-08-07 12:08:50 +0700

    Add libray into main to study on string

---
+++ Exercise_Resolve_again/Ex000_DoulicateStringInArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_Resolve_again
+{
+    public class DoulicateStringInArray
+    {
+
+        public DoulicateStringInArray()
+        {
+
+        }
+
+        public static readonly decimal pi = 3.14M;
+
+    }
+}
--- Exercise_Resolve_again/Program.cs
+++ Exercise_Resolve_again/Program.cs
@@ -15,6 +15,26 @@ namespace ExerciseResolve
             int num = 153;
             Ex001_InformationOfNumber infoNum = new Ex001_InformationOfNumber(num);
             Console.WriteLine(infoNum.ToString());
+
+            dynamic lastName = "Chand";
+            dynamic dynAuthor = new Author();
+            Console.WriteLine(dynAuthor.FullNameMethod(lastName));
+            Console.ReadKey();
         }
+
+        public class Author
+        {
+            // Define a dynamic field.   
+            dynamic name;
+            // Define a dynamic property.  
+            dynamic NameProperty { get; set; }
+            // Define a dynamic method with a dynamic parameter type.   
+            public dynamic FullNameMethod(dynamic d)
+            {
+                name = "Mahesh";
+                dynamic firstname = name;
+                return firstname + " " + d;
+            }
+        }  
     }
 }
